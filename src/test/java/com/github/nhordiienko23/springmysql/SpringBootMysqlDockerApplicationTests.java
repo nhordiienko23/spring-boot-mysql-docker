c68317b4e2737fa3fa5dc8f6eb291c45552d45e6
Nikita Hordiienko <nhordiienko23@gmail.com>
2026-07-04 18:31:13 +0300

    feat: add docker-compose file for MySQL and application.properties

---
+++ src/test/java/com/github/nhordiienko23/springmysql/SpringBootMysqlDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nhordiienko23.springmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMysqlDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
