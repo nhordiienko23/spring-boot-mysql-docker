5e6c2c952573c99ee53daa6865ec06eaed2cfcf3
Nikita Hordiienko <nhordiienko23@gmail.com>
2026-07-23 17:49:35 +0300

    test: refactor AbstractIntegrationTest to use singleton Testcontainer and add DirtiesContext

--- src/test/java/com/github/nhordiienko23/springmysql/AbstractIntegrationTest.java
+++ src/test/java/com/github/nhordiienko23/springmysql/AbstractIntegrationTest.java
@@ -3,28 +3,31 @@ package com.github.nhordiienko23.springmysql;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@Testcontainers
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class AbstractIntegrationTest {
 
-    @Container
-    static MySQLContainer<?> mySQLContainer= new MySQLContainer<>("mysql:8.0")
+
+    static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0")
             .withDatabaseName("spring_test_database")
             .withUsername("root")
             .withPassword("root");
 
+    static {
+        mySQLContainer.start();
+    }
 
     @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry){
-        registry.add("spring.datasource.url",mySQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.username",mySQLContainer::getUsername);
-        registry.add("spring.datasource.password",mySQLContainer::getPassword);
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
     }
 }
